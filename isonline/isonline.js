201b1509e327ab944cb2e3b70cbef05ae0a8f40a
bladerunner2020 <pivovarov@gmail.com>
2016-03-11 23:11:28 +0300

    Inital commit

---
+++ isonline/isonline.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Bladerunner on 11/03/16.
+ */
+
+var isOnline = require('is-online');
+
+module.exports = function(RED) {
+    function NodeIsOnline(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+            isOnline(function(err, online) {
+                msg.payload = online;
+                node.send(msg);
+
+            });
+
+
+        });
+    }
+
+    RED.nodes.registerType("Is-Online",NodeIsOnline);
+};
---
+++ readme.md
@@ -0,0 +1,12 @@
+node-red-contrib-isonline
+========================
+
+A <a href="http://nodered.org" target="_new">Node-RED</a> node to check if the Internet connection is up. 
+This node is a wrapping for <a href="http://nodejs.org" target="_new">Node JS</a> module <a href="https://www.npmjs.com/package/is-online" target="_new">is-online</a>.
+
+Install
+-------
+Run command on Node-RED installation directory
+
+	nam install node-red-contrib-isonline
+
