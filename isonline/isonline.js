f4d349703b53243cd59da9b12616db9b5aa52ff5
bladerunner2020 <pivovarov@gmail.com>
2016-03-12 23:09:46 +0300

    Changed connection method back to sockets. It can work with ip addresses and ports as well now.

--- isonline/isonline.js
+++ isonline/isonline.js
@@ -1,6 +1,9 @@
 /**
  * Created by Bladerunner on 11/03/16.
  */
+var net = require('net');
+var Promise = require('bluebird');
+
 module.exports = function(RED) {
     function NodeIsOnline(config) {
         RED.nodes.createNode(this,config);
@@ -10,23 +13,20 @@ module.exports = function(RED) {
 
         this.on('input', function(msg) {
             msg.timestamp = +new Date();
-            checkInternet(node.url, function(online) {
-                msg.online = online;
-                switch (parseInt(node.action)) {
-                    case 0:
-                        msg.payload = online;
-                        break;
-                    case 1:
-                        if (!online) msg = null;
-                        break;
-                    case 2:
-                        if (online) msg = null;
-                        break;
-                }
 
-                node.send(msg);
+            var url = node.url || 'google.com';
+            var pos = url.indexOf(":");
+            var port = "80";
+            if (pos > 0) {
+                port = url.substring(pos+1);
+                url = url.substring(0, pos);
+            }
+
+            checkConnection(url, port).then(function() {
+                SendMessage(node, msg, true);
+            }, function(err) {
+                SendMessage(node, msg, false);
             });
-
 
         });
     }
@@ -34,13 +34,41 @@ module.exports = function(RED) {
     RED.nodes.registerType("Is-Online",NodeIsOnline);
 };
 
-function checkInternet(url, cb) {
-    var url_to_check = url || 'google.com';
-    require('dns').lookup(url_to_check, function(err) {
-        if (err && err.code == "ENOTFOUND") {
-            cb(false);
-        } else {
-            cb(true);
-        }
-    })
+function SendMessage(node, msg, online) {
+
+    msg.online = online;
+    switch (parseInt(node.action)) {
+        case 0:
+            msg.payload = online;
+            break;
+        case 1:
+            if (!online) msg = null;
+            break;
+        case 2:
+            if (online) msg = null;
+            break;
+    }
+
+    node.send(msg);
 }
+
+function checkConnection(host, port, timeout) {
+    return new Promise(function(resolve, reject) {
+        timeout = timeout || 1000;     // default of 10 seconds
+        var timer = setTimeout(function() {
+            reject("timeout");
+            socket.end();
+        }, timeout);
+        var socket = net.createConnection(port, host, function() {
+            clearTimeout(timer);
+            resolve();
+            socket.end();
+        });
+        socket.on('error', function(err) {
+            clearTimeout(timer);
+            reject(err);
+        });
+    });
+}
+
+
